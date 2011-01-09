6dc95d756c72ef875604f85007bf14da8a998557
QU Zongyao <zongyao.qu@gmail.com>
2011-01-09 19:44:02 +0900

    only use local version for the version number

--- MPlayerX/version.sh
+++ MPlayerX/version.sh
@@ -7,17 +7,12 @@ if [ $LOCALVER \> 1 ] ; then
     VER=`git rev-list origin/master | sort | join config.git-hash - | wc -l | awk '{print $1}'`
 
     if [ $VER != $LOCALVER ] ; then
-        VER="$VER+$(($LOCALVER-$VER))"
+        VER=$LOCALVER
     fi
-    
+
 	## 1028 is the base of the svn version
     VER=`expr ${VER} + 1028`
-    
-    if git status | grep -q "modified:" ; then
-        VER="${VER}.1"
-    else
-    	VER="${VER}.0"
-    fi
+
     echo $VER
 
 else
