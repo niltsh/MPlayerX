7446ab36ba911e408d5b5932abcf23172875048b
QU Zongyao <zongyao.qu@gmail.com>
2011-05-14 17:37:54 +0900

    add Greek

---
+++ MPlayerX/MPApplication.h
@@ -0,0 +1,16 @@
+//
+//  MPApplication.h
+//  MPlayerX
+//
+//  Created by 瞿 宗耀 on 11-5-14.
+//  Copyright 2011年 MPlayerX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MPApplication : NSApplication {
+    
+}
+
+@end
---
+++ MPlayerX/MPApplication.m
@@ -0,0 +1,14 @@
+//
+//  MPApplication.m
+//  MPlayerX
+//
+//  Created by 瞿 宗耀 on 11-5-14.
+//  Copyright 2011年 MPlayerX. All rights reserved.
+//
+
+#import "MPApplication.h"
+
+
+@implementation MPApplication
+
+@end
