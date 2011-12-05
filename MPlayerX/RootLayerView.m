85b5c12f68190e727e5073bc88a7467e8d6ded6b
Zongyao Qu <zongyao.qu@gmail.com>
2011-12-05 22:54:03 +0900

    fix ControlUI does not show up in full screen

--- MPlayerX/RootLayerView.m
+++ MPlayerX/RootLayerView.m
@@ -1295,7 +1295,10 @@ float AreaOf(NSPoint p1, NSPoint p2, NSPoint p3, NSPoint p4)
 }
 
 -(void) windowDidEnterFullScreen:(NSNotification *)notification
-{	
+{
+    // It seams the fullscreen window is another window, so I have to set accept mouseMovedEvents
+    // otherwise, View could not receive the events
+    [[self window] setAcceptsMouseMovedEvents:YES];
 	[[self window] makeFirstResponder:self];
 
 	NSSize sz = [self bounds].size;
